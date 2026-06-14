1c30d31c111a38e70c207e87040cede347bf57d7
Alexis Cristian <alexis.filho@dcomp.ufs.br>
2026-06-13 22:16:02 -0300

    Implementa método de remoção de músicas na playlist

--- Playlist.java
+++ Playlist.java
@@ -22,4 +22,48 @@ public class Playlist {
         ultimaMusica.getAnterior().setProxima(ultimaMusica);
         ultimaMusica.getProxima().setAnterior(ultimaMusica);
     }
+
+    public void removerMusica(String nomeMusica) {
+        if (estaVazia()) {
+            System.out.println("Playlist vazia");
+            return;
+        }
+        NoMusica encontrado = null;
+        NoMusica percorre = ultimaMusica.getProxima();
+        do {
+            if (percorre.getNome() == nomeMusica) {
+                encontrado = percorre;
+                break;
+            }
+        } while (percorre != ultimaMusica.getProxima());
+        if (ultimaMusica == ultimaMusica.getProxima() && encontrado != null) {
+            ultimaMusica = null;
+            return;
+        }
+        if (encontrado == ultimaMusica) {
+            ultimaMusica = ultimaMusica.getAnterior();
+            ultimaMusica.setAnterior(ultimaMusica.getAnterior().getAnterior());
+            ultimaMusica.setProxima(ultimaMusica.getProxima().getProxima());
+            return;    
+        }
+        if (encontrado != null) {
+            encontrado.getAnterior().setProxima(encontrado.getProxima());
+            encontrado.getProxima().setAnterior(encontrado.getAnterior());
+            return;
+        }
+    }
+
+    public void exibirPlaylist() {
+        if (estaVazia()) {
+            System.out.println("Playlist vazia");
+            return;
+        }
+        NoMusica percorre = ultimaMusica.getProxima();
+        do {
+            System.out.print(percorre + " // ");
+            percorre = percorre.getProxima();
+        } while (percorre != ultimaMusica.getProxima());
+        System.out.println();
+    }
+    
 }
